d1db3d3c42a82e5fe31c1057ec9f25f07d6888e8
rachelcwk <theracheleckels@gmail.com>
2021-02-26 22:51:37 +0000

    Attempted Debug

--- main.py
+++ main.py
@@ -9,22 +9,33 @@ size = (width, height) = (int(screen_info.current_w), int(screen_info.current_h)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 color = (0, 127, 255)
-
-
-
+fishes = []
 
 def main():
+  for i in range(10):
+    fishes.append(Fish((width / 2, height / 2)))
   while True:
     clock.tick(60)
     for event in pygame.event.get():
       if event.type == QUIT:
         sys.exit()
+      if event.type == MOUSEBUTTONDOWN:
+        fishes.append(Fish(event.pos))
+      if event.type == KEYDOWN:
+        if event.key == K_d:
+          for i in range(len(fishes) // 2):
+            fishes.pop(0)
+        if event.key == K_f:
+          pygame.display.set_mode(screen_size, FULLSCREEN)
+        if event.key == K_ESCAPE:
+          pygame.display.set_mode(size)
     screen.fill(color)
-    fish.update()
-    fish.draw(screen)
-    screen.blit(fish_image, fish_rect)
+    for fish in fishes:
+      fish.update()
+    for fish in fishes:
+      fish.draw(screen)
     pygame.display.flip()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
   main()
